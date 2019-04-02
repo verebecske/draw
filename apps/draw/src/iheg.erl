4447477869fc1b0092f462c22fe00502807833d6
verebecske <julia.csuha@erlang-solutions.com>
2019-04-02 20:54:13 +0200

    Update iheg.erl

--- apps/draw/src/iheg.erl
+++ apps/draw/src/iheg.erl
@@ -2,5 +2,11 @@
 -export([all]).
 
 -graph(Data,Opt) -> 
-  create(Size),
-  
+  Image = create(Size),
+  NewData = change_things(Data,Opt),
+  NewImage = add_lines(NewData, Image),
+  save(NewImage).
+
+add_lines([],Image) -> Image;
+add_lines(Data,Image) ->
+  add_lines(lists:tail(Data),Image).
