58a41ec626599f83058277c997b787d3c1191c9b
verebecske <julia.csuha@erlang-solutions.com>
2019-04-03 12:56:31 +0200

    lovely graph

--- apps/draw/src/egraph.erl
+++ apps/draw/src/egraph.erl
@@ -5,14 +5,13 @@ init() ->
 	L1 = {test1, [{1,1}, {2,2}, {8,7}, {10,13}]},
 	L2 = {test2, [{1,11},{2,21},{5,30},{10,50}]},
 	L3 = {test3, [{1,5}, {2,13},{5,35},{10,70}]},
-	graph([L1,L2,L3],opt),
-	ok.
+	graph([L1,L2,L3],opt).
 
 graph(Data,Opt) -> 
 	Image = create(Opt),
 	NewData = change_position(Data,Opt),
 	NewImage = add_lines(NewData,Image),
-	save(NewImage).
+	save(Image).
 
 add_lines(Data,Image) -> 
 	add_lines(Data,Image,0).
@@ -21,7 +20,7 @@ add_lines([ {Name, Points} | Data],Image,Number) ->
 	Color = color(Number),
 	make_label(Name, Image, Color),
 	make_line(Points,Image, Color),
-	add_lines(Data,Image).
+	add_lines(Data,Image,Number+1).
 
 make_line([Point],Image,Color) -> Image;
 make_line([P0,P1 | Points], Image, Color) ->
@@ -32,15 +31,21 @@ make_line([P0,P1 | Points], Image, Color) ->
 create(Opt) ->
 	Width = 600,
 	Height = 300,
+	MW = trunc(Width / 10),
+	MH = trunc(Height / 10),
 	Image = egd:create(Width,Height),
 	Color = egd:color(silver),
-	egd:line(Image,50,Width-50, Color),
-	egd:line(Image,50,Height-50,Color),
-	make_number(Opt),
+	P0 = {MW,MH},
+	P1 = {MW,Height-MH},
+	P2 = {Width-MW,Height-MH},
+	egd:line(Image,P0,P1,Color),
+	egd:line(Image,P1,P2,Color),
+%	make_number(Opt),
 	Image.
 
  %choose color and use an algoritm. I don't no yet what algoritm
 color(Number) ->  
+	List = [black, silver, gray, white, maroon, red, purple, fuchia, green, lime, olive, yellow, navy, blue, teal, aqua],
 	Color = case Number of
 		0 -> green;
 		1 -> blue;
@@ -50,9 +55,9 @@ color(Number) ->
 	egd:color(Color).
 
 save(Image) ->
-	Count = 0,
+	Count = 2,
 	Png = egd:render(Image, png, [{render_engine, opaque}]),
-	FileName = "Wgraph" ++ erlang:integer_to_list(Count) ++ ".png",
+	FileName = "wgraph" ++ erlang:integer_to_list(Count) ++ ".png",
 	egd:save(Png, FileName),
     egd:destroy(Image),
     Png.
