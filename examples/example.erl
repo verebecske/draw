87c4eaa56155d29219893d2f8196d606b7278bce
verebecske <julia.csuha@erlang-solutions.com>
2019-05-21 14:10:41 +0200

    example

--- examples/example.erl
+++ examples/example.erl
@@ -2,10 +2,59 @@
 -export([run/0]).
 
 run() ->
-    tester1(),
-    tester2(). 
+    basic(),
+    one_line(),
+    points(),
+    three_line(),
+    example1(),
+    example2().
 
-tester1() -> 
+basic() -> 
+    Data = [ 
+            { basic, [ {1,0} ] } ],
+    Filename = generate_filename("doc","basic3.png"),
+    Labels = ["1"],
+    Unit = "Hello",
+    OptsMap = #{width => 800, height => 500, margin_width => 80, 
+                margin_height => 90, main_label => "Basic"},
+    wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
+
+one_line() -> 
+    Data = [ { basic, [ {0,0},{1,1} ] } ],
+    Filename = generate_filename("doc","one_line3.png"),
+    Labels = ["{0,0}","{1,1}"],
+    Unit = "Hello",
+    OptsMap = #{width => 800, height => 500, margin_width => 80, 
+                margin_height => 90, main_label => "One line"},
+    wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
+
+points() -> 
+    Data = [ 
+            {one,  [ {0,0}, {90,50} ] }, 
+            {two,  [ {0,10}, {20,20}, {40,70}, {80,95} ] },
+            {three,[ {0,0}, {10,20}, {30,50}, {60,15}, {100,90} ] }
+           ],
+    Filename = generate_filename("doc","points3.png"),
+    Labels = ["0","10","30","60","100"],
+    Unit = "Hello",
+    OptsMap = #{width => 800, height => 500, margin_width => 80, 
+                margin_height => 90, main_label => "Points"},
+    wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
+
+three_line() ->
+    Data = [ 
+            {one,  [ {0,0},   {20,10}, {40,40}, {60,10}, {80,40}, {100,50} ] }, 
+            {two,  [ {0,20}, {20,70}, {40,50}, {60,40}, {80,50}, {100,60} ] },
+            {three,[ {0,90}, {20,30}, {40,60}, {60,80}, {80,60}, {100,30} ] }
+           ],
+    Filename = generate_filename("doc","three_line.png"),
+    Labels = ["0","20","40","60","80","100"],
+    Unit = "Hello",
+    OptsMap = #{width => 800, height => 500, margin_width => 80, 
+                margin_height => 90, main_label => "Graph"},
+    wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
+
+example1() -> 
     Data = [ 
             { test1, [ {0,0}, {10, 40}, {20, 50}, {30, 69}, {40,76}, {50,94}] },
             { test2, [ {0,0}, {10, 10}, {20, 25}, {30, 30}, {40,45}, {50,60}] }
@@ -14,10 +63,10 @@ tester1() ->
     Labels = ["0","10","20","30","40","50"],
     Unit = "Hello",
     OptsMap = #{width => 800, height => 500, margin_width => 80, 
-                margin_height => 90, main_label => "Test 1"},
+                margin_height => 90, main_label => "Example 1"},
     wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
 
-tester2() -> 
+example2() -> 
     Data = [ 
             {one,  [ {0,0},   {20,100}, {40,900}, {60,400}, {80,500}, {100,700} ] }, 
             {two,  [ {0,500}, {20,400}, {40,700}, {60,600}, {80,500}, {100,600} ] },
@@ -28,7 +77,7 @@ tester2() ->
     Labels = ["0","20","40","60","80","100"],
     Unit = "Hello",
     OptsMap = #{width => 800, height => 500, margin_width => 80, 
-                margin_height => 90, main_label => "Test 2"},
+                margin_height => 90, main_label => "Example 2"},
     wgraph:graph(Data,Labels,Unit,Filename,OptsMap).
 
 generate_filename(Path,Filename) ->
